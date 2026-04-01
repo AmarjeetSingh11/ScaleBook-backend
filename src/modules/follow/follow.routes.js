0f434d0fb8860febd2b56eacb2820935b59954fd
AmarjeetSingh11 <amarjeetsingh25111@gmail.com>
2026-04-02 00:27:35 +0530

    created the requestlist-api and update-request api

--- src/modules/follow/follow.routes.js
+++ src/modules/follow/follow.routes.js
@@ -5,5 +5,7 @@ const router = express.Router();
 
 router.get('/get-list-of-users-to-follow', authenticate, followController.getListofUsersToFollow);
 router.post('/send-request/:followingId', authenticate, followController.sendRequestToFollow);
+router.get('/pending-to-follow-requests', authenticate, followController.getListOfRequestsToFollow);
+router.patch('/follow-requests/:requestId/status', authenticate, followController.updateFollowRequestStatus);
 
 export default router;
